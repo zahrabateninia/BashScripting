b26ceaf8b09455e6e41362e39fc4aa67d72c8c53
zahrabateninia <zahrabatenin@gmail.com>
2023-11-02 15:04:07 +0330

    Delete meeting_reminder.sh

--- meeting_reminder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-meeting_info = $(zenity--forms\
-    --title 'Meeting' --text 'Reminder information'\
-    --add-calender 'Date' --add-entry 'Title'\
-    --add-entry 'Emails'\
-    2>/dev/null)
